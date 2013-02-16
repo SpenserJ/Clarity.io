1a1168a74693bc8240979e493b5174225f7f8f9e
Spenser Jones <spenser.e.jones@gmail.com>
2013-02-16 07:31:15 +0100

    Basic support for Knockout

--- public/js/clarity.js
+++ public/js/clarity.js
@@ -1,3 +1,18 @@
+var Clarity = function Clarity() {
+  var self = this;
+
+  self.torrents = ko.observableArray([
+    new Torrent({ index: 1, name: 'Example Torrent', size: '124.5MB', progress: { status: 'Seeding', percent: 100.0 } })
+  ]);
+};
+
+var Torrent = function Torrent(data) {
+  this.data = data;
+}
+
+var clarity = new Clarity();
+ko.applyBindings(clarity);
+
 var socket = io.connect();
 
 socket.on('initialize', function (data) {
